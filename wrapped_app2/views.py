5fc719bc16f39134f8946c3be1c1105135f50b71
sbharucha3 <shanayabharucha23@gmail.com>
2024-10-24 16:41:24 -0400

    able to access token using my personal client id and secret

---
+++ wrapped_app2/views.py
@@ -0,0 +1,31 @@
+#from django.shortcuts import render
+import base64
+from requests import post
+import json
+
+
+# Create your views here.
+client_id = 'defb4c242b7247529ae6409bc5ab3a7f'
+client_secret = 'f32301833f4a4f85b2cf4caed8bd4f7d'
+
+def get_token():
+    auth_string = client_id + ':' + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        'Authorization': 'Basic ' + auth_base64,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result['access_token']
+    return token
+
+
+token = get_token()
+print(token)
+
+
