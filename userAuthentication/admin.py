aa636bf0e24f9bd16e7cd91498e4ea98dd4a257e
erosenfeld2005 <emmyrosenfeld@gmail.com>
2024-10-28 19:46:19 -0400

    added a name field to the sign up page so we can use that when making wraps for users

--- userAuthentication/admin.py
+++ userAuthentication/admin.py
@@ -1,8 +1,17 @@
-from django.contrib import admin
-
-# Register your models here.
+# admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    # Specify the fields to display in the admin list view
+    list_display = ('username', 'email', 'name', 'is_staff')
+
+    # Specify the fields to edit in the detail view
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('name',)}),  # Add the name field here
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
