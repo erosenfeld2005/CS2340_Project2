2b34d4d7e972692f4e8c57318ecc8902a5cc03b4
erosenfeld2005 <emmyrosenfeld@gmail.com>
2024-10-28 13:40:13 -0400

    still under dev adding name field to login

--- userAuthentication/models.py
+++ userAuthentication/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
 class CustomUser(AbstractUser):
+    name = models.CharField(max_length=150, blank=False, default="User")
     groups = models.ManyToManyField(
         Group,
         related_name='customuser_set',  # Ensure this is unique
