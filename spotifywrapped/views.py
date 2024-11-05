b1e9d51b2e131b6b809915d9d8265ac97af6cab5
ayoun8 <150699484+ayoun8@users.noreply.github.com>
2024-11-05 13:46:16 -0500

    Fixed everything. Working account settings, working dark mode (WIP), working deletion (confirmation, front end and back end), working slides 6 and 7

--- spotifywrapped/views.py
+++ spotifywrapped/views.py
@@ -2,7 +2,8 @@
 Python file that renders the landing page when the website is opened
 """
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 
 def landing_page(request):
     """
@@ -30,3 +31,26 @@ def summary(request):
     :return: The rendered summary.html page.
     """
     return render(request, 'summary.html')
+
+@login_required
+def account_settings(request):
+    return render(request, 'deletion/account_settings.html')
+
+@login_required
+def confirm_delete_account(request):
+    return render(request, 'deletion/confirm_delete_account.html')
+
+@login_required
+def account_deleted(request):
+    """
+    View to display a message that the account has been successfully deleted.
+    """
+    return render(request, 'deletion/account_deleted.html')
+
+@login_required
+def delete_account_confirmed(request):
+    if request.method == 'POST':
+        request.user.delete()
+        logout(request)
+        return redirect('signup')
+    return redirect('account_settings')
