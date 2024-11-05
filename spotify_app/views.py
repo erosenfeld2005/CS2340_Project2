01334d55898fcc0ed657262f7b6fdcc8f10a18e4
ayoun8 <150699484+ayoun8@users.noreply.github.com>
2024-11-05 03:09:00 -0500

    Pushing

--- spotify_app/views.py
+++ spotify_app/views.py
@@ -176,6 +176,7 @@ def display_summary_content(request):
                 "vibe_data": temp_profile.vibe_data,  # Pass vibe data
                 "temp_profile_id": temp_profile_id
             }
+
             return render(request, 'summary.html', context)
 
         except TemporarySpotifyProfile.DoesNotExist:
